1f87e5634e3e177d3f3a0a8b14fff3196131b8fd
Ameyzing-1nonly <137088431+Ameyzing-1nonly@users.noreply.github.com>
2025-10-05 22:16:28 +0530

    parent dash

---
+++ src/pages/Dashboards/Parents_Dashboard.jsx
@@ -0,0 +1,12 @@
+
+
+const AdhdDashboard = () => {
+  return (
+    <div>
+      <h1>ADHD Dashboard</h1>
+      <p>Welcome to the ADHD Dashboard!</p>
+    </div>
+  );
+};
+
+export default AdhdDashboard;
